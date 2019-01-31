ad09ef8681adc490f29d006ab13808905fb5f071
JamesCrimp <James.Crimp@gmail.com>
2019-02-01 10:03:41 +1300

    Monkey ids, multiple instances of monkey

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <div>
       <h1>React development has begun!</h1>
-      <Monkey />
+      <Monkey id='1' />
+      <Monkey id='2' />
+      <Monkey id='3' />
     </div>
   )
 }
