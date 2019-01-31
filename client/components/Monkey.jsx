04cea86df146ab40e6322834bfb5ba72a102bed6
Rebecca Jackson <zimbecs@gmail.com>
2019-02-01 11:32:33 +1300

    animate buttons

--- client/components/Monkey.jsx
+++ client/components/Monkey.jsx
@@ -22,9 +22,9 @@ class Monkey extends React.Component{
         return this.state.alive ? (
             <div className={`monkey${id}area`}>
                 <div className={`bananasButton banana${id}`}>
-                    <button onClick={this.eat.bind(this)}>
+                    <div onClick={this.eat.bind(this)}>
                         <img src="/images/bananas.png" alt="Bananas"/>
-                    </button>
+                    </div>
                 </div>
                 <div className={`monkey${id} bounce-out-bottom`}>
                     <img  src={`/images/monkey${id}.png`} alt="Curious George"/>
