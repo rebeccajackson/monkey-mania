df48e766998c359168d30a3eec3527749de5d531
Rebecca Jackson <zimbecs@gmail.com>
2019-01-31 17:46:04 +1300

    added stylesheet and data files

---
+++ data/monkeys.js
@@ -0,0 +1,24 @@
+const monkeys = {
+  monkeys:[
+    {
+      id: 1,
+      name: 'alan',
+      img_url: '/images/barry.jpg',
+      state: 5
+    },
+    {
+      id: 2,
+      name: 'barry',
+      img_url: '/images/barry.jpg',
+      state: 5
+    },
+    {
+      id: 3,
+      name: 'craig',
+      img_url: '/images/barry.jpg',
+      state: 5
+    }
+  ]
+}
+
+export default monkeys
